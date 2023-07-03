d38deb59b6838bb0538a8a4c382a004dd27925dc
ilyashmackov <i.shmackov@mail.ru>
2023-07-03 17:59:44 +0300

    First commit

---
+++ src/components/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from '../todoList/todoList';
+
+const App = () => {
+    return (
+        <div>
+            <TodoList/>
+        </div>          
+    )
+}
+
+export default App;
---
+++ src/components/todoList/todoList.tsx
@@ -0,0 +1,16 @@
+import '../../index.css'
+import React from 'react';
+
+const TodoList = () => {  
+  
+    return (
+      <div className="component">
+        <div className="todo-list">{12}</div>
+      <div className="controls">
+                  
+        </div>
+      </div>
+      
+    )}
+
+    export default TodoList;
